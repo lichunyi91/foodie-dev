757fbbf23ccb74b4f3fc2ad302c1a07a83e67dbf
licy <15810481589@139.com>
2019-11-18 11:47:14 +0800

    two

--- foodie-dev-api/src/main/java/com/imooc/Application.java
+++ foodie-dev-api/src/main/java/com/imooc/Application.java
@@ -14,6 +14,7 @@ import java.sql.SQLOutput;
 //45
 public class Application {
     public static void main(String[] args) {
+        System.out.println("two");
         SpringApplication.run(Application.class,args);
     }
 }
