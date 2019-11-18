991c2f6001f8c0e4276ae5245e8ff1eb9306a2ee
licy <15810481589@139.com>
2019-11-18 11:19:45 +0800

    three

--- foodie-dev-api/src/main/java/com/imooc/Application.java
+++ foodie-dev-api/src/main/java/com/imooc/Application.java
@@ -6,12 +6,16 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
 import tk.mybatis.spring.annotation.MapperScan;
 
+import java.sql.SQLOutput;
+
 @SpringBootApplication
 @MapperScan(basePackages = "com.imooc.mapper")
 @ComponentScan(basePackages="com.imooc")
 //456567
 public class Application {
     public static void main(String[] args) {
+
+        System.out.println("123");
         SpringApplication.run(Application.class,args);
     }
 }
