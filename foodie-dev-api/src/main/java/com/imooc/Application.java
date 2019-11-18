91ff020f122906d87074022a820e1fdfdfa624a6
licy <15810481589@139.com>
2019-11-18 11:28:18 +0800

    master

--- foodie-dev-api/src/main/java/com/imooc/Application.java
+++ foodie-dev-api/src/main/java/com/imooc/Application.java
@@ -11,7 +11,7 @@ import java.sql.SQLOutput;
 @SpringBootApplication
 @MapperScan(basePackages = "com.imooc.mapper")
 @ComponentScan(basePackages="com.imooc")
-//456567
+//45
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class,args);
