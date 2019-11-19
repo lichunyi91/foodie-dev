c8bed04705a5ccfa68ac124af9104ef9066c4047
licy <15810481589@139.com>
2019-11-19 10:55:41 +0800

    master

--- foodie-dev-api/src/main/java/com/imooc/controller/StuFooController.java
+++ foodie-dev-api/src/main/java/com/imooc/controller/StuFooController.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
 
+@ApiIgnore
 @RestController
 public class StuFooController {
     @Autowired
